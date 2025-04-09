262c49a56b4e127167aafa4776c57eb2695c4558
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-09 02:06:11 -0700

    update to 0.12.10

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,30 +5,25 @@ set -o xtrace -o nounset -o pipefail -o errexit
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
-./mill -i show dev.assembly
-install -m 755 out/dev/assembly.dest/mill ${PREFIX}/bin/mill
+./mill -i show dist.assembly
+install -m 755 out/dist/assembly.dest/out.jar ${PREFIX}/libexec/${PKG_NAME}/mill.jar
+
+tee ${PREFIX}/bin/mill << EOF
+#!/bin/sh
+exec \${JAVA_HOME}/bin/java -jar \${CONDA_PREFIX}/libexec/mill/mill.jar "\$@"
+EOF
 
 # Create batch wrapper so that it has a .cmd extension and is recognized as executable
 tee ${PREFIX}/bin/mill.cmd << EOF
-call %CONDA_PREFIX%\libexec\mill\mill %*
+call %JAVA_HOME%\bin\java -jar %CONDA_PREFIX%\libexec\mill\mill.jar %*
 EOF
 
-./mill -i show dev.publishM2Local ${SRC_DIR}/m2
+./mill -i show dist.publishM2Local --m2RepoPath ${SRC_DIR}/m2
 pom_file=$(find ${SRC_DIR}/m2 -name "*.pom")
 mv ${pom_file} $(dirname ${pom_file})/pom.xml
 
 cd $(dirname ${pom_file})
-
-# Add exclusion for build-only dependency which does not provide an upstream JAR
-tee exclusions.xml << EOF
-<exclusions>
-    <exclusion>
-        <groupId>com.kohlschutter.junixsocket</groupId>
-        <artifactId>junixsocket-core</artifactId>
-    </exclusion>
-</exclusions>
-EOF
-sed -i "s?<dependency>?<dependency>$(tr -d '\n' < exclusions.xml)?" pom.xml
+sed -i "s/SNAPSHOT/${PKG_VERSION}/g" pom.xml
 
 # Download licenses and move them to ${SRC_DIR}
 mvn license:download-licenses -Dgoal=download-licenses
